56c16947d4e9620fe2026ff118b6468a03067a5d
Jesse Paris <Jesse.Paris@qlik.com>
2016-09-29 17:03:22 -0700

    Code cleanup and bug fix for Approved/Not approved.

--- plugins/sheetapprover/sheetapprover.component.js
+++ plugins/sheetapprover/sheetapprover.component.js
@@ -1,209 +1,180 @@
-(function(){
+(function () {
     "use strict";
     var module = angular.module("QMCUtilities")
 
 
-    function fetchTableHeaders($http){
+    function fetchTableHeaders($http) {
         return $http.get("/sheetapprover/data/tableDef.json")
-        .then(function(response)
-        {
-            return response.data;
-        });
+            .then(function (response) {
+                return response.data;
+            });
     }
 
-    function fetchTableRows($http)
-    {
+    function fetchTableRows($http) {
         return $http.get('/sheetapprover/getSheets')
-        //return $http.get("data/testData.json")
-        .then(function(response)
-        {
-            return response.data;
-        });
+            //return $http.get("data/testData.json")
+            .then(function (response) {
+                return response.data;
+            });
     }
 
-    function approveSheets($http, sheetIds)
-    {
+    function approveSheets($http, sheetIds) {
         console.log('running approveSheets');
         return $http.post('/sheetapprover/approveSheets', sheetIds)
-        .then(function(response)
-        {
-            return response;
-        });
+            .then(function (response) {
+                return response;
+            });
     }
 
-    function unapproveSheets($http, sheetIds)
-    {
-        return $http.post('/sheetapprover/unapproveSheets',sheetIds)
-        .then(function(response)
-        {
-            return response;
-        });
+    function unapproveSheets($http, sheetIds) {
+        return $http.post('/sheetapprover/unapproveSheets', sheetIds)
+            .then(function (response) {
+                return response;
+            });
     }
 
-    function sheetBodyController($http)
-    {
+    function sheetBodyController($http) {
         var model = this;
         var colNames = [];
-        model.columnNames= [];
+        model.columnNames = [];
         model.tableRows = []
         model.outputs = [];
         model.searchSheets = '';
 
 
-        model.$onInit = function() {
-            fetchTableHeaders($http).then(function(table)
-            {
-                model.columnNames = table.columns;
-            })
-            .then(function()
-            {
-                fetchTableRows($http).then(function(response)
-                {
-                    model.tableRows = response.rows;
-                });   
-            });
+        model.$onInit = function () {
+            fetchTableHeaders($http).then(function (table) {
+                    model.columnNames = table.columns;
+                })
+                .then(function () {
+                    fetchTableRows($http).then(function (response) {
+                        model.tableRows = response.rows;
+                    });
+                });
         };
 
-        model.highlight = function(string, searchString)
-        {
+        model.highlight = function (string, searchString) {
             console.log(string);
             console.log(searchString);
-           /* if(!searchString)
-            {
-                //$sce.trustAsHtml(string);
-            }
-            return $sce.trustAsHtml(string.replace(new RegExp(searchString, "gi"), function(match)
-            {
-                return '<span class="lui-texthighlight">' + match + '</span>'
-            })) */
+            /* if(!searchString)
+             {
+                 //$sce.trustAsHtml(string);
+             }
+             return $sce.trustAsHtml(string.replace(new RegExp(searchString, "gi"), function(match)
+             {
+                 return '<span class="lui-texthighlight">' + match + '</span>'
+             })) */
         }
 
-        model.checkme = function(checkme)
-        {
-            if(checkme)
-            {
-               return true;
-            }
-            else
-            {
+        model.checkme = function (checkme) {
+            if (checkme) {
+                return true;
+            } else {
                 return false;
             }
         }
 
-        model.getWidth = function(){
-            $(".th-column").each(function()
-            {
+        model.getWidth = function () {
+            $(".th-column").each(function () {
                 console.log($(this).css('width'));
             });
         };
 
-        model.setValue = function(checkme, $index, sheetId)
-        {
-            if(checkme)
-            {
+        model.setValue = function (checkme, $index, sheetId) {
+            if (checkme) {
                 model.outputs.push(sheetId);
-            }
-            else
-            {
+            } else {
                 var index = model.outputs.indexOf(sheetId);
-                model.outputs.splice(index,1);
+                model.outputs.splice(index, 1);
             }
             console.log(model.outputs);
         };
 
-        model.approve = function()
-        {
+        model.approve = function () {
             approveSheets($http, model.outputs)
-            .then(function(response)
-            {
-                console.log(response);
-                if(response.data.success)
-                {
-                    $('.approveUnapprove').prop('checked', false);
-                    model.outputs = [];
-                    response.data.items.forEach(function(item)
-                    {
-                        model.tableRows.forEach(function(row, index)
-                        {
-                            if(item.id==row[7])
-                            {
-                                model.tableRows[index][2] = item.approved;
-                               // model.checkme(false);
-
-                            }
+                .then(function (response) {
+                    console.log(response);
+                    if (response.data.success) {
+                        $('.approveUnapprove').prop('checked', false);
+                        model.outputs = [];
+                        response.data.items.forEach(function (item) {
+                            model.tableRows.forEach(function (row, index) {
+                                if (item.id == row[7]) {
+                                    if (item.approved == true) {
+                                        model.tableRows[index][2] = "Approved";
+                                    }
+                                    else if (item.approved == false) {
+                                        model.tableRows[index][2] = "Not approved";
+                                    }
+                                    // model.checkme(false);
+                                }
+                            });
                         });
-                    });
-                }
-            });
+                    }
+                });
         };
 
-        model.unapprove = function()
-        {
+        model.unapprove = function () {
             unapproveSheets($http, model.outputs)
-            .then(function(response)
-            {
-                console.log(response);
-                if(response.data.success)
-                {
-                    $('.approveUnapprove').prop('checked', false);
-                    model.outputs = [];
-                    response.data.items.forEach(function(item)
-                    {
-                        model.tableRows.forEach(function(row, index)
-                        {
-                            if(item.id==row[7])
-                            {
-                                model.tableRows[index][2] = item.approved;   
-                            }
+                .then(function (response) {
+                    console.log(response);
+                    if (response.data.success) {
+                        $('.approveUnapprove').prop('checked', false);
+                        model.outputs = [];
+                        response.data.items.forEach(function (item) {
+                            model.tableRows.forEach(function (row, index) {
+                                if (item.id == row[7]) {
+                                    if (item.approved == true) {
+                                        model.tableRows[index][2] = "Approved";
+                                    }
+                                    else if (item.approved == false) {
+                                        model.tableRows[index][2] = "Not approved";
+                                    }
+                                }
+                            });
                         });
-                    });
-                }
-            });
+                    }
+                });
         };
 
     }
 
-    function controller(){
+    function controller() {
         var model = this;
 
-        model.supportClick = function()
-        {
+        model.supportClick = function () {
             alert("I'm a button!");
         };
 
-        model.$onInit = function(){
+        model.$onInit = function () {
             model.message = "I AM THE SHEET APPROVER";
         };
     }
 
     module.component("supportStatement", {
-        templateUrl:"plugins/sheetapprover/support-statement.component.html" 
+        templateUrl: "plugins/sheetapprover/support-statement.component.html"
     });
 
     module.component("sheetApproverBody", {
         transclude: true,
-    
-        templateUrl:"plugins/sheetapprover/sheet-approver-body.html",
+
+        templateUrl: "plugins/sheetapprover/sheet-approver-body.html",
         controllerAs: "model",
         controller: ["$http", sheetBodyController]
     });
-   
-   module.filter('highlight',function()
-   {
-       return function(text, search)
-       {
-           if(text && search)
-           {
-            text = text.toString();
-            search = search.toString();
-            return text.replace(new RegExp(search, 'gi'), '<span class="lui-texthighlight">$&</span>');
-           }
-           else {
-               return text;
-           }
 
-       }
-        
-   });
+    module.filter('highlight', function () {
+        return function (text, search) {
+            if (text && search) {
+                text = text.toString();
+                search = search.toString();
+                return text.replace(new RegExp(search, 'gi'), '<span class="lui-texthighlight">$&</span>');
+            } else {
+                return text;
+            }
+
+        }
+
+    });
 
 }());
