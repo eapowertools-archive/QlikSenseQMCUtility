9ede2eeeb00c93242a725ef79c8b06f4bb028dc0
Jeff Goldberg <goldbergjeffrey@yahoo.com>
2016-12-09 12:29:29 -0500

    updated batch file for creating the service dispatcher entry in services

--- tmp/createServiceDispatcher.bat
+++ tmp/createServiceDispatcher.bat
@@ -1,3 +1,13 @@
 sc query state=all | find "QlikEAPowerToolsServiceDispatcher"
-IF %ERRORLEVEL% EQU 1 sc create QlikEAPowerToolsServiceDispatcher binPath= "%~1\PowerToolsServiceDispatcher\PowerToolsService.exe" DisplayName= "Qlik EAPowerTools Service Dispatcher" start= auto obj=%2 password=%3
-pause
+
+IF %ERRORLEVEL% EQU 1
+GOTO ADDSERVICE
+ELSE
+GOTO END
+
+:ADDSERVICE
+sc create QlikEAPowerToolsServiceDispatcher binPath= "%~1\PowerToolsServiceDispatcher\PowerToolsService.exe" DisplayName= "Qlik EAPowerTools Service Dispatcher" start= auto obj=%2 password=%3
+sc description "QlikEAPowerToolsServiceDispatcher" "Service Dispatcher for running EA Powertools" 
+
+:END
+exit
